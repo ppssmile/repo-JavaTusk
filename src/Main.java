32cd6cea513273e8d82f55db582df423e37df105
ppssmile <ppssmile1@gmail.com>
2020-11-15 01:56:21 +0300

    modification Main.java adn .gitignore

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println();
+        String name = new String("Pavel is good student!");
+        System.out.println(name);
     }
 }
